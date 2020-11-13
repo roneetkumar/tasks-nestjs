b2b5bb6e14e601f8f8858f213c797179708814fc
Roneet Kumar <roneetparida@gmail.com>
2020-11-13 01:34:34 -0500

    user repository

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authServise: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authCredentialsDTO: AuthCredentialsDTO,
+  ): Promise<void> {
+    return this.authServise.signUp(authCredentialsDTO);
+  }
+}
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,29 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDTO: AuthCredentialsDTO): Promise<void> {
+    const { username, password } = authCredentialsDTO;
+
+    const user = new User();
+
+    user.username = username;
+    user.password = password;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+}
